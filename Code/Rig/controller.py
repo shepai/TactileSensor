be2a9404c367c459793c64816d57ecab693ac2f6
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-09-26 15:34:53 +0100

    Update controller.py

--- Code/Rig/controller.py
+++ Code/Rig/controller.py
@@ -3,24 +3,9 @@ from mpremote import pyboard
 import serial, time
 import sys
 import glob
-import letRun #This library can be deleted, it is used for debugging
-import RoboSkin as sk
 import numpy as np
 import pickle 
 
-SIZE=0.3
-name="C:/Users/dexte/OneDrive/Documents/AI/Data_Labeller/pickle_imputer.pkl"
-reg=None
-with open(name,'rb') as file:
-    reg=pickle.load(file)
-
-def predict(reg1,dat):
-    p=reg1.predict(dat)
-    p=(p.reshape((p.shape[0],p.shape[1]//2,2))*255/SIZE)
-    return p
-"""
-Setup control with micropython device
-"""
 
 class Board:
     def __init__(self):
@@ -67,7 +52,7 @@ class Board:
             except (OSError, serial.SerialException):
                 pass
         return result
-    def autoConnect(self,file="C:/Users/dexte/OneDrive/Documents/GitHub/TactileSensor/Code/TactileSensor/boardSide.py"):
+    def autoConnect(self,file="C:/Users/dexte/Documents/GitHub/TactileSensor/Code/TactileSensor/boardSide.py"):
             COM=""
             while COM=="":
                 try:
@@ -94,63 +79,46 @@ class experiment:
         self.sensor=sensor
         path=""
         if sys.platform.startswith('win'):
-            path="C:/Users/dexte/OneDrive/Documents/GitHub/TactileSensor/Code/Rig/boardSide.py"
+            path="C:/Users/dexte/Documents/GitHub/TactileSensor/Code/Rig/boardSide.py"
         else:
             path="/its/home/drs25/Documents/GitHub/TactileSensor/Code/Rig/boardSide.py"
         self.control=Board()
         self.control.autoConnect(path) #autoconnect and run file
 
-    def moveTillTouch(self,threshold=5):
+    def moveTillTouch(self,threshold=300):
         touched=False
+        #get moving
+        for i in range(100):
+            s=self.sensor.getSensor(type_="round")
         while not touched:
             s=self.sensor.getSensor(type_="round")
-            print(s)
+            #print(np.average(s),self.control.getWeight())
             self.control.moveZ(-1)
-            if np.average(s)>threshold: touched=True
+            if self.control.getWeight()>threshold: touched=True
     def moveZ(self,cm,dir): #dir must be 1 or -1
         assert dir==1 or dir==-1, "Incorrect direction, must be 1 or -1"
         cm=cm*17 #17 steps per cm
         for i in range(0,round(cm)):
-            self.b.moveZ(1*dir) #move up
+            self.control.moveZ(1*dir) #move up
     def moveX(self,cm,dir): #dir must be 1 or -1
         assert dir==1 or dir==-1, "Incorrect direction, must be 1 or -1"
         cm=cm*26 #26 steps per cm
         for i in range(0,round(cm)):
-            self.b.moveX(1*dir) #move up
+            self.control.moveX(1*dir) #move up
     def pressures(self,cm_samples=2,step=0.5):
         a=[]
-        Image=self.skin.getBinary() #get initial image
-        #self.skin.reset()
-        #self.old_T=self.skin.origin
-        self.move_till_touch(Image) #be touching the platform
+        d=[]
+        self.moveTillTouch() #be touching the platform
         for i in np.arange(0, cm_samples, step):
             #print("depth:",i)
             mag=self.sensor.getSensor(type_="round")
             time.sleep(1)
             self.moveZ(i,-1) #move down
             time.sleep(1)
-            mag=self.sensor.getSensor(type_="round")
-            a.append(mag)
+            data=self.sensor.getSensor(type_="round")
+            a.append(data)
+            d.append(self.control.getWeight())
             self.moveZ(i,1) #move back
         self.moveZ(1,1) #move back
-        return np.array(a)
-    def pressures(self,cm_samples=2,step=0.5):
-        a=[]
-        Image=self.skin.getBinary() #get initial image
-        #self.skin.reset()
-        #self.old_T=self.skin.origin
-        self.move_till_touch(Image) #be touching the platform
-        
-        for i in range(num_samples):
-            self.moveZ(0.5,-1) #move down
-            self.moveX(1,-1)
-            data=self.sensor.getSensor(type_="round")
-            l_dt[i]=np.sum(vectors,axis=0)/len(vectors)
-            time.sleep(1)
-            self.moveZ(0.5,1) #move up
-            self.moveX(1,1)
-            time.sleep(1)
-        return np.array(a)
-
-
-
+        return np.array(a),np.array(d)
+    
