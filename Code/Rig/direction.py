aa140c7c7df7152a956109f5a351f639f6e49f9d
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-10-10 17:28:31 +0100

    Update direction.py

--- Code/Rig/direction.py
+++ Code/Rig/direction.py
@@ -16,14 +16,14 @@ exp=controller.experiment(B)
 trials=3
 steps=4
 samples=100
-data=np.zeros((samples,trials,steps,10))
+data=np.zeros((samples,trials,2,steps,10))
 t1=time.time()
 try:
     for i in range(samples):
         print("Complete:",round((i/samples)*100,1),"%","Time taken:",round((time.time()-t1)/60,1),"minutes")
         sensor=exp.direction(trials,steps)
         data[i]=sensor
-        np.save("C:/Users/dexte/Documents/GitHub/TactileSensor/Code/Data collection/pressures/directionVals_2",data)
+        np.save("C:/Users/dexte/Documents/GitHub/TactileSensor/Code/Data collection/pressures/directionVals_1",data)
 except KeyboardInterrupt:
     pass
 
