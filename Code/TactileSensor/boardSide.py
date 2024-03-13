8ae9442539261f15328a73bfd4fdefe5deb6a735
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-03-13 14:41:52 +0200

    simple control

--- Code/TactileSensor/boardSide.py
+++ Code/TactileSensor/boardSide.py
@@ -1,48 +1,15 @@
 import machine
 import time
+from Tactile_MP import *
 
+foot=Foot([0,1,2,3],26,4)
 # Define the control pins
-S0 = machine.Pin(0,machine.Pin.OUT)  # GP0
-S1 = machine.Pin(1,machine.Pin.OUT)  # GP1
-S2 = machine.Pin(2,machine.Pin.OUT)  # GP2
-S3 = machine.Pin(3,machine.Pin.OUT)  # GP3
-SIG = machine.ADC(26)  # GP26
-
-SE = machine.Pin(4,machine.Pin.OUT)  # GP3
-SE.value(0)
-a=[]
-b=[]
-
-# Function to select a channel on the multiplexer
-def select_channel(channel):
-    channel=f'{channel:04b}'
-    S0.value(int(channel[3]))
-    S1.value(int(channel[2]))
-    S2.value(int(channel[1]))
-    S3.value(int(channel[0]))
-
-
 def gather(low_pass=True,high_pass=True,alpha=0.2):
-    global a
-    global b
-    global UT
-    array=[]
-    untouched=[]
-    for i in range(16):
-        select_channel(i)
-        value = SIG.read_u16()  # Read the analog value
-        untouched.append(value)
-        if low_pass:
-            value=(1-alpha)*a[i] + (alpha*value) #low pass filter
-        if high_pass:
-            value=alpha*b[i] + alpha*(value-a[i])
-        array.append(value)
-    print([float('{:f}'.format(untouched[i])) for i in range(len(array))])
-    a=array.copy()
-    b=array.copy()
-    UT=untouched.copy()
-    return array
-
-a=gather(low_pass=False,high_pass=False)
-b=gather(low_pass=False,high_pass=False)
-UT=gather(low_pass=False,high_pass=False)
+    foot.alpha=alpha
+    data=foot.read()
+    if low_pass:
+        data=foot.low_pass
+    elif low_pass:
+        data=foot.high_pass
+    print([float('{:f}'.format(data[i])) for i in range(len(data))])
+    return data
